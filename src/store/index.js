77a1626f2930672c2bfee9ec5a85a76b848ecc07
Paco Preciado Mendoza <__pm@outlook.com>
2019-03-09 18:08:06 -0600

    :sparkles: store was added

---
+++ src/store/index.js
@@ -0,0 +1,9 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import promiseMiddleware from "redux-promise";
+import reducers from "./../reducers";
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export const store = createStore(
+  reducers, {}, composeEnhancer(applyMiddleware(promiseMiddleware))
+)
