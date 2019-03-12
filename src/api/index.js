24b2e2dd8c038a1137c62b6afd348bd7329f7a6f
Paco Preciado Mendoza <__pm@outlook.com>
2019-03-12 15:40:37 -0600

    :sparkles: apiPut to update any customer was added

--- src/api/index.js
+++ src/api/index.js
@@ -1,1 +1,16 @@
 export const apiGet = url => () => fetch(url).then(r => r.json())
+
+export const apiPut = (url, id, obj) => () =>
+  fetch(`${url}/${id}`,
+    {
+      method: "PUT",
+      body: JSON.stringify(obj),
+      headers: new Headers({ "Content-type": "application/json" })
+    }
+  )
+    .then(res => res.json())
+    .then(r => {
+      if (r.error) {
+        return Promise.reject(r.validation);
+      }
+    })
